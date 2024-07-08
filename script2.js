ca073b5d0ec1aa937fe602cc61f4c23bbaf4b525
Bobur816 <boburabduxalilov1999@gmail.com>
2024-07-08 15:51:18 +0500

    fixed-navbar

--- script2.js
+++ script2.js
@@ -10,4 +10,17 @@ $(document).ready(function () {
 
     parent.classList.toggle("active");
   });
+
+  function toggleMobileNav() {
+    $(".navigation__list")[0].classList.toggle("active");
+    $(".navigation__mobile_bg")[0].classList.toggle("active");
+  }
+
+  $(".btn_of_navbar-mobile").on("click", toggleMobileNav);
+
+  $(".navigation__list").on("click", function (e) {
+    if (e.target.classList.contains("navigation__list-item__link")) {
+      toggleMobileNav();
+    }
+  });
 });
