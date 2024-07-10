271a1d8e84581275a4b77ed42004778529fb2e25
Bobur816 <boburabduxalilov1999@gmail.com>
2024-07-10 23:45:33 +0500

    'navbar-fontfamily-fix'

--- script2.js
+++ script2.js
@@ -1,15 +1,23 @@
-$(document).ready(function () {
-  $(".question__item__question").on("click", function (e) {
-    let parent = e.target.closest(".question__item");
-
-    document.querySelectorAll(".question__item").forEach((el) => {
-      if (el !== parent) {
-        el.classList.remove("active");
-      }
-    });
-
-    parent.classList.toggle("active");
+$(window).on("load", () => {
+  $(document).on("scroll", function () {
+    let rootFont = parseInt($(":root").css("font-size"));
+    let scroll = $(window).scrollTop();
+    scroll > rootFont * 5
+      ? $(".navigation").addClass("navigation-scrolled")
+      : $(".navigation").removeClass("navigation-scrolled");
   });
+
+  // $(".question__item__question").on("click", function (e) {
+  //   let parent = e.target.closest(".question__item");
+
+  //   document.querySelectorAll(".question__item").forEach((el) => {
+  //     if (el !== parent) {
+  //       el.classList.remove("active");
+  //     }
+  //   });
+
+  //   parent.classList.toggle("active");
+  // });
 
   function toggleMobileNav() {
     $(".navigation__list")[0].classList.toggle("active");
@@ -21,6 +29,18 @@ $(document).ready(function () {
   $(".navigation__list").on("click", function (e) {
     if (e.target.classList.contains("navigation__list-item__link")) {
       toggleMobileNav();
+    }
+  });
+
+  $(".question__item").click(function () {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this).find(".question__item__answer").slideUp(400);
+    } else {
+      $(".question__item").removeClass("active");
+      $(".question__item__answer").slideUp(400);
+      $(this).addClass("active");
+      $(this).find(".question__item__answer").slideDown(400);
     }
   });
 });
